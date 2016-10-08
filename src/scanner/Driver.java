c83fb4399c013991adc97edd7aed459672ef4189
Christopher Porch <cbporch@gmail.com>
2016-10-08 11:49:29 -0400

    Added LICENSE, NOTICE, "lib" directory and some jBCrypt test code

--- src/scanner/Driver.java
+++ src/scanner/Driver.java
@@ -1,7 +1,24 @@
 package scanner;
 
+/*
+ * Copyright 2016 Christopher Porch, Christopher Deck, Steve Leonetti, Tom Miller, Dan Smith, Mike Bayruns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -16,9 +33,10 @@ public class Driver {
     public static void main(String args[]) throws IOException {
         System.out.println("Not Broken");
 
+
         StemAnalyzer analyzer = new StemAnalyzer();
         TokenStream ts = analyzer.tokenStream("field",
-                new StringReader("this is a test string, to display the features of Lucene"));
+                new StringReader("this is a test string, to display the features of Lucene after running"));
         CharTermAttribute termAtt = ts.addAttribute(CharTermAttribute.class);
         try {
             ts.reset();
@@ -29,6 +47,9 @@ public class Driver {
         } finally {
             ts.close();
         }
+
+        String hashed1 = BCrypt.hashpw("pass", BCrypt.gensalt(11));
+        System.out.println(hashed1 + "\n" + BCrypt.checkpw("password", "$2a$11$GHUCFlb6.D/pNu8nFBNAsOEjgqadbkht.0FgtsrfHNpRuY7Pyli86"));
 
 
     }
--- src/scanner/StemAnalyzer.java
+++ src/scanner/StemAnalyzer.java
@@ -14,16 +14,32 @@ import java.util.List;
 
 /**
  * Created by Chris Porch on 10/7/2016.
- *
+ * <p>
  * This class extends the Standard Analyzer from the Lucene analysis library
- * and adds in stemming functionality to reduce English words to their root words
+ * and adds in stemming functionality
+ */
+
+/*
+ * Copyright 2016 Christopher Porch, Christopher Deck, Steve Leonetti, Tom Miller, Dan Smith, Mike Bayruns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 public class StemAnalyzer extends Analyzer {
-    public static final CharArraySet ENGLISH_STOP_WORDS_SET;
-    public static final int DEFAULT_MAX_TOKEN_LENGTH = 255;
+    private static final CharArraySet ENGLISH_STOP_WORDS_SET;
+    private static final int DEFAULT_MAX_TOKEN_LENGTH = 255;
     private int maxTokenLength;
-    public static final CharArraySet STOP_WORDS_SET;
+    private static final CharArraySet STOP_WORDS_SET;
 
     public StemAnalyzer() {
         this(STOP_WORDS_SET);
@@ -38,8 +54,8 @@ public class StemAnalyzer extends Analyzer {
         src.setMaxTokenLength(this.maxTokenLength);
         StandardFilter tok = new StandardFilter(src);
         LowerCaseFilter tok1 = new LowerCaseFilter(tok);
-        final StopFilter tok2 = new StopFilter(tok1, STOP_WORDS_SET);
-        EnglishMinimalStemFilter tok3 = new EnglishMinimalStemFilter(tok2); // this is the relevant part
+        StopFilter tok2 = new StopFilter(tok1, STOP_WORDS_SET);
+        final EnglishMinimalStemFilter tok3 = new EnglishMinimalStemFilter(tok2); // this is the relevant part
         return new TokenStreamComponents(src, tok3) {
             protected void setReader(Reader reader) {
                 src.setMaxTokenLength(maxTokenLength);
@@ -49,9 +65,9 @@ public class StemAnalyzer extends Analyzer {
     }
 
     static {
-        List stopWords = Arrays.asList(new String[]{"a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if",
+        List stopWords = Arrays.asList("a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if",
                 "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then",
-                "there", "these", "they", "this", "to", "was", "will", "with"});
+                "there", "these", "they", "this", "to", "was", "will", "with");
         CharArraySet stopSet = new CharArraySet(stopWords, false);
         ENGLISH_STOP_WORDS_SET = CharArraySet.unmodifiableSet(stopSet);
         STOP_WORDS_SET = ENGLISH_STOP_WORDS_SET;
