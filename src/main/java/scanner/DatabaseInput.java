f2e88bc76c58911d28d2e8e725458ab8413c93ab
Chris Deck <cdeck20@optonline.net>
2016-10-10 14:26:55 -0400

    Added .java file for database input

--- src/main/java/scanner/DatabaseInput.java
+++ src/main/java/scanner/DatabaseInput.java
@@ -71,6 +71,9 @@ public class DatabaseInput {
                     word = word.replaceAll("\\s+", "");
                     System.out.println(word);
                 }
+
+                //have lucene run through the inputs to take out filler words before going into the database
+                //word with number is more confidential
             }
         });
     }
