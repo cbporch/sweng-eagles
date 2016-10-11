c308e0d780e3bd3bc2b62ce2889aa3b0f7a053c0
Christopher Porch <cbporch@gmail.com>
2016-10-11 11:41:49 -0400

    Made Hasher work more or less

--- src/main/java/scanner/filter/Hasher.java
+++ src/main/java/scanner/filter/Hasher.java
@@ -2,11 +2,27 @@ package scanner.filter;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.util.ArrayList;
+
 /**
  * Created by Chris on 10/11/2016.
  * Class uses jBCrypt to hash a given string
  */
 public class Hasher {
 
+    public Hasher(){
+
+    }
+
+    /*
+     * Hashes an ArrayList of strings using a salt generated using the default number of rounds
+     */
+    public ArrayList<String> hash(ArrayList<String> list){
+        ArrayList<String> hashedList = new ArrayList();
+        for(String word: list){
+            hashedList.add(BCrypt.hashpw(word, BCrypt.gensalt()));
+        }
+        return hashedList;
+    }
 
 }
