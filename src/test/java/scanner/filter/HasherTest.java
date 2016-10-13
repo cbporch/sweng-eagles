69b1f388f321319d731decf85e58be489a384c82
TomMiller95 <t.miller1995@yahoo.com>
2016-10-13 18:17:41 -0400

    updated unit tests

--- src/test/java/scanner/filter/HasherTest.java
+++ src/test/java/scanner/filter/HasherTest.java
@@ -1,16 +1,28 @@
 package scanner.filter;
 
 import org.junit.Test;
-
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 /**
  * Created by Chris on 10/11/2016.
  */
 public class HasherTest {
-    @Test
-    public void testHash() throws Exception {
 
+    @Test
+    /**
+     * This will test to see if the words are being hashed
+     */
+    public void testHash() throws Exception {
+        ArrayList<String> inputList = new ArrayList<String>();
+        inputList.add("The");
+        inputList.add("dog");
+        inputList.add("barked");
+        Hasher hasher = new Hasher();
+        ArrayList<String> outputList = hasher.hash(inputList);
+        assertEquals("Hashed the", outputList.get(0));
+        assertEquals("Hashed dog", outputList.get(1));
+        assertEquals("Hashed barked", outputList.get(2));
     }
 
 }
