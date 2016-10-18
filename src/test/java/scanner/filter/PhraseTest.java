f948d508ad7ff98407c980c875e0841c47e65c88
cbporch <cbporch@gmail.com>
2016-10-18 16:04:24 -0400

    Added JUnit test for Phrase class

---
+++ src/test/java/scanner/filter/PhraseTest.java
@@ -0,0 +1,30 @@
+package scanner.filter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chris on 10/18/16.
+ *
+ */
+public class PhraseTest {
+    private Phrase testPhrase;
+    @Before
+    public void setUp() throws Exception {
+        testPhrase = new Phrase("test text", 2);
+    }
+
+    @Test
+    public void getPhrase() throws Exception {
+        Assert.assertTrue(testPhrase.getPhrase().equals("test text"));
+    }
+
+    @Test
+    public void getWordcount() throws Exception {
+        Assert.assertEquals(2, testPhrase.getWordcount());
+    }
+
+}
