28ed4a554ba10d094015c0360e311300dd420008
Chris Deck <cdeck20@optonline.net>
2016-10-19 15:43:19 -0400

    reworking database input gui

--- src/main/java/scanner/dbEntry/DatabaseInput.java
+++ src/main/java/scanner/dbEntry/DatabaseInput.java
@@ -7,6 +7,7 @@ import scanner.filter.Phrase;
 import scanner.filter.StringToHash;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
@@ -77,14 +78,109 @@ public class DatabaseInput {
         });
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         JFrame frame = new JFrame("DatabaseInput");
         frame.setContentPane(new DatabaseInput().container);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+       *//* //Create the frame.
+        JFrame frame = new JFrame("FrameDemo");
+        //Set what happens when the frame closes?
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //3. Create components and put them in the frame.
+        //...create emptyLabel...
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        //Size the frame.
+        frame.pack();
+        //Show it.
+        frame.setVisible(true);*//*
+    }*/
+
+    public static void addComponentsToPane(Container pane) {
+        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+        JPanel instructionsPanel = new JPanel();
+        JLabel instructions = new JLabel("Enter the words/phrases to be inputted below");
+        instructionsPanel.add(instructions);
+        pane.add(instructionsPanel);
+
+        // words input panel
+        JPanel wordsInputPanel = new JPanel();
+        wordsInputPanel.setBackground(Color.BLACK);
+        pane.add(wordsInputPanel, new BoxLayout(pane, BoxLayout.Y_AXIS));
+        JLabel wordsLabel = new JLabel("Words");
+        wordsInputPanel.add(wordsLabel);
+
+        //words text field and options
+        JPanel wordOptions = new JPanel();
+        wordsInputPanel.add(wordOptions, new BoxLayout(pane, BoxLayout.X_AXIS));
+        JTextField wordsTextField = new JTextField();
+        JRadioButton synBtn = new JRadioButton("Synonyms?");
+        JRadioButton numDependentBtn = new JRadioButton("# Dependent?");
+        JTextField probField = new JTextField();
+        //look into hint text
+        wordOptions.add(wordsTextField);
+        wordOptions.add(synBtn);
+        wordOptions.add(numDependentBtn);
+        wordOptions.add(probField);
+
+        // phrases input panel
+        JPanel phrasesInputPanel = new JPanel();
+        phrasesInputPanel.setBackground(Color.BLACK);
+        pane.add(wordsInputPanel, new BoxLayout(pane, BoxLayout.Y_AXIS));
+        JLabel phrasesLabel = new JLabel("Phrases");
+        wordsInputPanel.add(phrasesLabel);
+
+        //phrase text field and options
+
+        JPanel phraseOptions = new JPanel();
+        phrasesInputPanel.add(wordOptions, new BoxLayout(pane, BoxLayout.X_AXIS));
+        JTextField phraseTextField = new JTextField();
+        JRadioButton phraseSynBtn = new JRadioButton("Synonyms?");
+        JRadioButton phraseNumDependentBtn = new JRadioButton("# Dependent?");
+        JTextField phraseProbField = new JTextField();
+        //look into hint text
+        phraseOptions.add(phraseTextField);
+        phraseOptions.add(phraseSynBtn);
+        phraseOptions.add(phraseNumDependentBtn);
+        phraseOptions.add(phraseProbField);
+
+        JPanel submitPanel = new JPanel();
+        JButton submitButton = new JButton("Submit");
+        final JLabel successLabel = new JLabel("Test");
+        submitPanel.add(submitButton);
+        submitPanel.add(successLabel);
+        pane.add(submitPanel, BorderLayout.SOUTH);
+
+        /*evaluateButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                scoreLabel.setText("45");
+            }
+        });*/
     }
 
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("DatabaseGUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setPreferredSize(new Dimension(500,400));
+        frame.setTitle("Database Input");
+        frame.setResizable(true);
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
     private void processInput(String[] words, String[] phrases) throws Exception {
         ArrayList<String>   stemmedWords = new ArrayList<String>(),
                 dbHashedWords,
@@ -176,6 +272,16 @@ public class DatabaseInput {
             e.printStackTrace();
         }
 
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
 
