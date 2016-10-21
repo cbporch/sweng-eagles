6c1cdc5020f6beccc0716c589064b8c022297f0a
Steve Leonetti <SteveGL09@gmail.com>
2016-10-21 18:43:27 -0400

    play test formatting adjustment

--- src/main/java/scanner/Doublet.java
+++ src/main/java/scanner/Doublet.java
@@ -1,13 +1,18 @@
+package scanner;
+
 public class Doublet
 {
 	private final int numConf = 0;
 	private final int numNorm = 0;
 	
 	
-	protected int getNumConf() {
+	protected int getNumConf()
+	{
 		return numConf;
 	}
-	protected int getNumNorm() {
+
+	protected int getNumNorm()
+	{
 		return numNorm;
 	}
 }
