3279e3e7aaaedfb8cb259a493788235847c3e5f5
Chris Deck <cdeck20@optonline.net>
2016-10-26 18:40:06 -0400

    commenting database input

--- src/main/java/scanner/dbEntry/DatabaseInput.java
+++ src/main/java/scanner/dbEntry/DatabaseInput.java
@@ -283,98 +283,6 @@ public class DatabaseInput {
         pane.add(submitPanel, BorderLayout.SOUTH);
     }
 
-    /*private static void processInput(String[] words, String[] phrases) throws Exception {
-        ArrayList<String>   stemmedWords,
-                dbHashedWords,
-                dbHashedPhrases,
-                unique_words = new ArrayList<>();
-        ArrayList<Phrase>   stemmedPhrases = new ArrayList<>(),
-                unique_phrases = new ArrayList<>();
-        LuceneStemmer ls = new LuceneStemmer();
-
-        try {
-            dbHashedWords = Database.getWords();
-            dbHashedPhrases = Database.getPhrases();
-
-            // move array into ArrayList for method call
-            ArrayList<String> w = new ArrayList<>(Arrays.asList(words));
-
-            stemmedWords = ls.stemWords(w);
-
-            if(stemmedWords.size() != 0) {
-                boolean duplicate = false, empty = true;
-                int count = 1;
-                System.out.print("Checking word ");
-                for (String inputWord : stemmedWords) {
-                    System.out.print(count++ + ", ");
-                    if (dbHashedWords != null) {
-                        for(String hash: dbHashedWords) {
-                            if (!duplicate && Hasher.checkHashBCrypt(inputWord, hash)) {
-                                // once a match is found, we no longer need to check each word
-                                duplicate = true; // should stop if statement from running when it hits a duplicate
-                            }
-                        }
-                    }
-
-                    if(!duplicate){ // word is not in database
-                        unique_words.add(inputWord);
-                        empty = false;
-                    }
-                    duplicate = false; // reset variable
-                }
-
-                if(!empty) {
-                    // hash unique words
-                    unique_words = StringToHash.getHashes(unique_words);
-                    for (String hashedWord : unique_words) {
-                        Database.insertWords(hashedWord, RARITY);
-                    }
-                    System.out.println("\nWords inserted");
-                }
-            }
-
-            // stem phrases before checking in database, maintaining word count for each phrase
-            for(String phrase: phrases){
-                stemmedPhrases.add(new Phrase(ls.stemPhrase(phrase), phrase.split("\\s+").length));
-            }
-
-            // find unique phrases in input
-            if(stemmedPhrases.size() != 0) {
-                boolean duplicate = false, empty = true;
-                int count = 1;
-                System.out.print("Checking phrase ");
-                for (Phrase inputPhrase : stemmedPhrases) {
-                    System.out.print(count++ + ", ");
-                    if (dbHashedPhrases != null) {
-                        for(String hash : dbHashedPhrases) {
-                            if (!duplicate && Hasher.checkHashBCrypt(inputPhrase.getPhrase(), hash)) {
-                                duplicate = true;
-                            }
-                        }
-                    }
-                    if(!duplicate){
-                        unique_phrases.add(inputPhrase);
-                        empty = false;
-                    }
-                }
-
-                if(!empty) {
-                    // hash unique phrases
-                    unique_phrases = StringToHash.getPhraseHashes(unique_phrases);
-                    for (Phrase phrase: unique_phrases) {
-                        Database.insertPhrases(phrase.getPhrase(), RARITY, phrase.getWordcount());
-                    }
-                    System.out.println("\nPhrases inserted");
-                }
-
-            }
-            System.out.println("Processing complete");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }*/
-
     public static void processInputSHA(String[] words, String[] phrases, double wordProb, double phraseProb, int wordNumDep, int phraseNumDep) throws Exception {
         ArrayList<String>   stemmedWords,
                             dbHashedWords,
@@ -507,6 +415,10 @@ public class DatabaseInput {
         return listOfWords;
     }
 
+    /**
+     * Launches the GUI.
+     * @param args - not necessary
+     */
     public static void main(String[] args) {
         //Create and set up the window.
         JFrame frame = new JFrame("Database Input");
