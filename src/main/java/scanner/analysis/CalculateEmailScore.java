7ab878de934445aed8ad3f15dbfab54d31180a06
TomMiller95 <t.miller1995@yahoo.com>
2016-11-14 18:55:28 -0500

    Merge branch 'CSV_Class_Update' of /Users/Tom/Desktop/sweng/sweng-eagles with conflicts.

--- src/main/java/scanner/analysis/CalculateEmailScore.java
+++ src/main/java/scanner/analysis/CalculateEmailScore.java
@@ -4,19 +4,43 @@ import scanner.Doublet;
 
 import java.util.ArrayList;
 
+/**
+ * Calculates, using Bayes Theorem, the score of the email based on its confidential terms.
+ */
 public class CalculateEmailScore
 {
-//	static ArrayList<Doublet> termProbabilityList = new ArrayList<>();
-	
+    /**
+     * Bayes Theorem of all confidential terms is multiplied together to return a total score of the email.
+     * @param termProbabilityList ArrayList of Doublets that store each confidential term's usages of each type.
+     * @return emailScore score of the email.
+     */
 	public static double calculate(ArrayList<Doublet> termProbabilityList)
 	{
+		double greatestConfProb = 0;
 		double emailScoreX = 1;
 		double emailScoreY = 1;
-		
+
+		if (termProbabilityList.size() == 0)
+			return 0.0;
+
 		for (Doublet term : termProbabilityList)
 		{
-			double wordConfProb = term.getNumConf() / (double) term.getNumNorm();
-			double wordNormProb = term.getNumNorm() / (double) term.getNumConf();
+			int totalConfNorm = term.getNumConf() + term.getNumNorm();
+			double wordConfProb;
+			double wordNormProb;
+
+			if (totalConfNorm == 0)
+				wordConfProb = 0.0;
+			else
+				wordConfProb = term.getNumConf() / (double) totalConfNorm;
+
+			if (greatestConfProb < wordConfProb)
+				greatestConfProb = wordConfProb;
+
+			if (totalConfNorm == 0)
+				wordNormProb = 0.0;
+			else
+				wordNormProb = term.getNumNorm() / (double) totalConfNorm;
 
 			if(wordConfProb > 1.0){
 				wordConfProb = 1.0;
@@ -26,11 +50,21 @@ public class CalculateEmailScore
 				wordNormProb = 1.0;
 			}
 
-			double wordProb = wordConfProb / (wordConfProb + wordNormProb);
+			double wordProb;
+			double totalProb;
+			if ((totalProb = wordConfProb + wordNormProb) == 0)
+				wordProb = 0;
+			else
+				wordProb = wordConfProb / totalProb;
 			emailScoreX *= wordProb;
 			emailScoreY *= 1 - wordProb;
 		}
-		
-		return (emailScoreX / (emailScoreX + emailScoreY));
+
+		double result = emailScoreX / (emailScoreX + emailScoreY);
+
+		if (greatestConfProb > result)
+			return greatestConfProb;
+
+		return result;
 	}
 }
--- src/main/java/scanner/analysis/EmailTextGUI.java
+++ src/main/java/scanner/analysis/EmailTextGUI.java
@@ -1,76 +1,157 @@
 package scanner.analysis;
 
+import scanner.LoginGUI;
+import scanner.Word;
+import scanner.dbEntry.CSVFileReader;
+import scanner.dbEntry.Database;
+import scanner.dbEntry.DatabaseInput;
+
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
+import java.io.File;
+import java.util.ArrayList;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 /**
  * Created by cdeck_000 on 10/19/2016.
  */
 
-
+/**
+ * Create a simple GUI to input an email to be scanned. A score for the email will be shown on screen.
+ */
 public class EmailTextGUI {
+    protected static Database db;
+
+    private static JButton uploadFileBtn = new JButton("Upload File");
+    /**
+     * Add the components to the GUI.
+     * @param pane - the pane for the GUI
+     */
     public static void addComponentsToPane(Container pane) {
         pane.setLayout(new BorderLayout());
         JPanel instructionsPanel = new JPanel();
         JLabel instructions = new JLabel("Enter the email text below");
+        instructionsPanel.setBackground(Color.LIGHT_GRAY);
         instructionsPanel.add(instructions);
         pane.add(instructionsPanel, BorderLayout.NORTH);
 
         JPanel textAreaPanel = new JPanel();
         textAreaPanel.setBackground(Color.LIGHT_GRAY);
-        JTextArea textArea = new JTextArea();
-        textArea.setBackground(Color.LIGHT_GRAY);
+        final JTextArea textArea = new JTextArea();
+        textArea.setBackground(Color.WHITE);
+        textArea.setMinimumSize(new Dimension(400,350));
+        textArea.setMaximumSize(new Dimension(400,350));
+        textArea.setPreferredSize(new Dimension(400,350));
         textArea.setLineWrap(true);
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+        textArea.setBorder(border);
+        textArea.requestFocusInWindow();
+
+        textArea.setMinimumSize(new Dimension(500, 200));
         textArea.setFont(new Font("Serif", Font.PLAIN, 16));
         textAreaPanel.add(textArea);
         pane.add(textAreaPanel, BorderLayout.CENTER);
 
         JPanel scoringPanel = new JPanel();
         JButton evaluateButton = new JButton("Evaluate Email");
-        final JLabel scoreLabel = new JLabel("Test");
-        JButton uploadFileBtn = new JButton("Upload File");
+        final JLabel scoreLabel = new JLabel("");
+        JButton importTermsBtn = new JButton("Import Terms");
         scoringPanel.add(evaluateButton);
         scoringPanel.add(uploadFileBtn);
+        scoringPanel.add(importTermsBtn);
         scoringPanel.add(scoreLabel);
 
         pane.add(scoringPanel, BorderLayout.SOUTH);
 
-        evaluateButton.addMouseListener(new MouseAdapter() {
+        evaluateButton.addActionListener(new ActionListener() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                scoreLabel.setText("45");
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String email = textArea.getText();
+                    TextParser textParser = new TextParser(email);
+                    long f = System.currentTimeMillis();
+
+                    double score = textParser.parse();
+                    scoreLabel.setText(score+"");
+                    System.out.print((System.currentTimeMillis() - f));
+
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
             }
         });
 
-        uploadFileBtn.addMouseListener(new MouseAdapter() {
+        uploadFileBtn.addActionListener(new ActionListener() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                scoreLabel.setText("65");
+            public void actionPerformed(ActionEvent e) {
+                //Handle open button action.
+                //Create a file chooser
+                final JFileChooser fc = new JFileChooser();
+                System.out.println("In action listener");
+                if (e.getSource() == EmailTextGUI.uploadFileBtn) {
+                    System.out.println("In first if");
+                    int returnVal = fc.showOpenDialog(uploadFileBtn);
+
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        System.out.println("In second if");
+                        File file = fc.getSelectedFile();
+                        //This is where a real application would open the file.
+                        System.out.println("Opening: " + file.getName() + ".%n");
+                        //ArrayList<Word> words = CSVFileReader.interpretCSVFile(file+"");
+                        CSVFileReader.interpretCSVFile(file+"");
+                        /*for(Word word: words){
+                            System.out.println(word.getWord());
+                        }*/
+                    } else {
+                        System.out.println("Open command cancelled by user.%n");
+                    }
+                }
+            }
+            //successLabel.setText("Feature not available yet.");
+        });
+
+        importTermsBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LoginGUI loginGUI = new LoginGUI();
+                loginGUI.main(null);
             }
         });
     }
 
-        /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
+    /**
+     * Create the GUI and show it.
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("EmailGUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setPreferredSize(new Dimension(500,400));
+        //frame.setLocationRelativeTo(null);
+        frame.setPreferredSize(new Dimension(500,500));
         frame.setTitle("Email Text Input");
         frame.setResizable(true);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize.width, screenSize.height);
         //Set up the content pane.
         addComponentsToPane(frame.getContentPane());
 
         //Display the window.
         frame.pack();
         frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                System.out.println("Frame closing...");
+                try {
+                    db.close();
+                } catch (Exception ex){
+                    System.out.println(ex);
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -79,6 +160,7 @@ public class EmailTextGUI {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
+                db = new Database();
             }
         });
     }
