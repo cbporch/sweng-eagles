358446e943cdb6dcc0afc99082feabbd9ba7f390
Dan Smith <smithd6@students.rowan.edu>
2016-12-06 11:50:07 -0500

    Sends file to server, process file, returns result

--- src/main/java/scanner/pandoc/Pandoc.java
+++ src/main/java/scanner/pandoc/Pandoc.java
@@ -1,17 +1,22 @@
 package scanner.pandoc;
 
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
+import com.jcraft.jsch.*;
 
 import javax.swing.*;
+import java.io.*;
+import java.util.Random;
 
 /**
  * Created by dsmith on 12/06/2016.
  */
 public class Pandoc {
 
-    public static void main(String[] args){
+    String workingDir = "/pandoc/tmp/";
+
+    public String convertFile(String localPath, String localFilename){
+
+        Random rand = new Random();
+        String remoteName = Integer.toString(rand.nextInt(3000))+".txt";
 
         try{
             JSch jsch=new JSch();
@@ -28,8 +33,6 @@ public class Pandoc {
                 );
             }
 
-
-
             String user="ubuntu";
             String host = "ec2-54-145-184-115.compute-1.amazonaws.com";
 
@@ -41,19 +44,86 @@ public class Pandoc {
             session.setConfig(config);
             /* End Insecure solution */
 
-
             session.connect();
 
-            Channel channel=session.openChannel("shell");
+            upload(session, localPath);
+            String output = toText(session, workingDir+localFilename, workingDir+remoteName);
 
-            channel.setInputStream(System.in);
-            channel.setOutputStream(System.out);
+            session.disconnect();
 
-            channel.connect();
+            return output;
         }
         catch(Exception e){
             System.out.println(e);
         }
+
+        return null;
+    }
+
+    public Channel openExecChannel(Session session,String command) throws IOException {
+        try {
+            ChannelExec channel=(ChannelExec)session.openChannel("exec");
+            channel.setCommand(command);
+            channel.connect();
+            return channel;
+        }
+        catch ( JSchException e) {
+            throw new IOException("could not open exec channel with command " + command,e);
+        }
+    }
+
+    public void upload(Session session, String path) {
+        try {
+
+            ChannelSftp channelSftp = (ChannelSftp)session.openChannel("sftp");
+            channelSftp.connect();
+            channelSftp.cd(workingDir);
+
+            File f1 = new File(path);
+            channelSftp.put(new FileInputStream(f1), f1.getName(), ChannelSftp.OVERWRITE);
+
+            channelSftp.exit();
+            //session.disconnect();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String read(Session session, String path) throws IOException
+    {
+        try {
+            ChannelSftp sftp = (ChannelSftp) session.openChannel("sftp");
+            sftp.connect();
+            sftp.cd(workingDir);
+            InputStream stream = sftp.get(path);
+            String message ="";
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+                message = org.apache.commons.io.IOUtils.toString(br);
+                // read from br
+            } finally {
+                stream.close();
+                sftp.exit();
+                return message;
+
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String toText(Session session, String path, String outputPath) throws IOException
+    {
+        openExecChannel(session, "cd "+workingDir);
+        openExecChannel(session, "pandoc -s -o "+outputPath+" "+path);
+
+        String file =  read(session, outputPath);
+
+        return file;
+
     }
 
 }
