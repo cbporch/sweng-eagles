51bf6ab3d8e5b6ff647854eb8d0ed30c8b7cb06d
TomMiller95 <t.miller1995@yahoo.com>
2016-12-07 00:25:58 -0500

    bug

--- src/main/java/scanner/filtering/Encryptor.java
+++ src/main/java/scanner/filtering/Encryptor.java
@@ -1,11 +1,26 @@
 package scanner.filtering;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import java.security.*;
+import java.security.spec.*;
+
 
 /**
  * Encrypts a word, or phrase, or a given list of either. Also has methods to decrypt any of those types of input too.
@@ -16,18 +31,82 @@ import javax.xml.bind.DatatypeConverter;
  */
 public class Encryptor {
 
-    private static String key = "THIS IS THE KEY "; //Unsure what to make the key and initial vector.
-    private static String iv = "1234567812345678";  //                  ^^^
+    //THESE TWO LINES ARE FOR THE AES STUFF AND WILL MOST LIKELY BE DELETED.
+    private static String AESkey = "THIS IS THE KEY "; //Unsure what to make the key and initial vector.
+    private static String AESiv = "1234567812345678";  //                  ^^^
+    //END OF AES STUFF
+
+
+
+
+
+
+
+                    //********** ALL RSA STUFF ***************\\
+
+    /**
+     * So for the generation of the public and private keys, I did this in the terminal.
+     * $ openssl genrsa -out keypair.pem 2048
+     * $ openssl rsa -in keypair.pem -outform DER -pubout -out public.der
+     * $ openssl pkcs8 -topk8 -nocrypt -in keypair.pem -outform DER -out private.der
+     * $ locate private.der
+     * $ sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+     * $ locate public.der
+     * $ sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+     */
+
+    public byte[] readFileBytes(String filename) throws IOException
+    {
+        Path path = Paths.get(filename);
+        return Files.readAllBytes(path);
+    }
+
+    public PublicKey readPublicKey(String filename) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(readFileBytes(filename));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(publicSpec);
+    }
+
+    public PrivateKey readPrivateKey(String filename) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(readFileBytes(filename));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+    public byte[] encrypt(PublicKey key, byte[] plaintext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(plaintext);
+    }
+
+    public byte[] decrypt(PrivateKey key, byte[] ciphertext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(ciphertext);
+    }
+                                //********** ALL RSA STUFF ***************\\
+
+
+
+
+
+
+
+    //UNDER THIS COMMENT IS ALL AES STUFF AND PROBABLY WONT BE USED...ITS THE AES STUFF
 
     /**
      * This method will take in a word and encrpyt it.
      * @param word the word to be encrypted.
      * @return the encrypted version of the word.
      */
-    public static String encrypt(String word) {
+    public static String AESencrypt(String word) {
         try {
-            IvParameterSpec initVec = new IvParameterSpec(iv.getBytes("UTF-8"));
-            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            IvParameterSpec initVec = new IvParameterSpec(AESiv.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(AESkey.getBytes("UTF-8"), "AES");
 
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, initVec);
@@ -46,10 +125,10 @@ public class Encryptor {
      * @param encryptedWord the encrypted word that needs to be decrypted.
      * @return the original version of the word.
      */
-    public static String decrypt(String encryptedWord) {
+    public static String AESdecrypt(String encryptedWord) {
         try {
-            IvParameterSpec initVec = new IvParameterSpec(iv.getBytes("UTF-8"));
-            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            IvParameterSpec initVec = new IvParameterSpec(AESiv.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(AESkey.getBytes("UTF-8"), "AES");
 
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, initVec);
@@ -68,12 +147,12 @@ public class Encryptor {
      * @param list list of words that need to be encrypted.
      * @return the encrypted list of words.
      */
-    public static ArrayList<String> encryptList(ArrayList<String> list)
+    public static ArrayList<String> AESencryptList(ArrayList<String> list)
     {
         ArrayList<String> encryptedList = new ArrayList<>();
         for(String word: list){
             if(!word.equals("") && word != null) {
-                encryptedList.add(encrypt(word));
+                encryptedList.add(AESencrypt(word));
             }
         }
         for (int i = 0; i < encryptedList.size(); i++)
@@ -88,12 +167,12 @@ public class Encryptor {
      * @param list list of encrypted terms that will be decrypted.
      * @return list of words in their decrypted form.
      */
-    public static ArrayList<String> decryptList(ArrayList<String> list)
+    public static ArrayList<String> AESdecryptList(ArrayList<String> list)
     {
         ArrayList<String> decryptedList = new ArrayList<>();
         for(String word: list){
             if(!word.equals("") && word != null) {
-                decryptedList.add(decrypt(word));
+                decryptedList.add(AESdecrypt(word));
             }
         }
         for (int i = 0; i < decryptedList.size(); i++)
--- src/test/java/scanner/filtering/EncryptorTest.java
+++ src/test/java/scanner/filtering/EncryptorTest.java
@@ -1,26 +1,66 @@
 package scanner.filtering;
 
 import org.junit.Test;
+
+import java.io.File;
 import java.util.ArrayList;
 import static org.junit.Assert.*;
 import org.junit.Assert;
-
+import java.security.PrivateKey;
+import java.security.PublicKey;
 /**
  * Created by Tom on 11/28/16.
  */
 public class EncryptorTest {
 
     @Test
+    public void test()
+    {
+        Encryptor e = new Encryptor();
+        try
+        {
+            String privateKeyLocation = "src" + File.separatorChar
+                    + "main" + File.separatorChar
+                    + "java" + File.separator
+                    + "scanner" + File.separatorChar
+                    + "filtering" + File.separatorChar
+                    + "private.der";
+            String publicKeyLocation = "src" + File.separatorChar
+                    + "main" + File.separatorChar
+                    + "java" + File.separator
+                    + "scanner" + File.separatorChar
+                    + "filtering" + File.separatorChar
+                    + "public.der";
+            PublicKey publicKey = e.readPublicKey(publicKeyLocation);
+            PrivateKey privateKey = e.readPrivateKey(privateKeyLocation);
+            byte[] message = "Hello World".getBytes("UTF8");
+            byte[] secret = e.encrypt(publicKey, message);
+            byte[] recovered_message = e.decrypt(privateKey, secret);
+            System.out.println(new String(recovered_message, "UTF8"));
+        }
+        catch (Exception ee)
+        {
+            ee.printStackTrace();
+        }
+    }
+
+
+
+
+
+    //******** AES TESTING **********\\
+
+   // @Test
     public void testEncrpytAndDecrypt()
     {
         Encryptor e = new Encryptor();
-        String test = e.encrypt("Tom is the coolest guy in the world!");
+        String test = e.AESencrypt("Tom is the coolest guy in the world!");
         Assert.assertNotEquals("Tom is the coolest guy in the world!",test);    //Should be encrypted word vs. the original word.
-        assertEquals("Tom is the coolest guy in the world!",e.decrypt(test));   //Now it should be decrypted and match.
+        assertEquals("Tom is the coolest guy in the world!",e.AESdecrypt(test));   //Now it should be decrypted and match.
         assertEquals("xWhtIIral+mT9vK3QdStJu/J8+vhc9vr2T1i58JGd0xsJr7Bp5KV1t//Gmsmqe/6",test);  //encrypted word, matching with expected outcome.
     }
 
-    @Test
+  //  @Test
     public void testEncrpytAndDecryptList()
     {
         Encryptor e = new Encryptor();
@@ -29,10 +69,10 @@ public class EncryptorTest {
         origList.add("The cat meows.");
         origList.add("The Tom complains.");
 
-        ArrayList<String> testList = e.encryptList(origList);   //Encrypting the list.
+        ArrayList<String> testList = e.AESencryptList(origList);   //Encrypting the list.
         Assert.assertNotEquals(testList, origList);  //Encrypted list should not equal the original list.
         assertEquals(testList.get(0),"j7684JewP1X0oW8IARDzWg==");   //First word in encrypted list, matching with expected outcome.
 
-        assertEquals(origList, e.decryptList(testList)); //The test list now matches because it was decrypted.
+        assertEquals(origList, e.AESdecryptList(testList)); //The test list now matches because it was decrypted.
     }
 }
