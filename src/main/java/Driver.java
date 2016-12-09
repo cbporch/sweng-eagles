0f5416d091b6d050db7d6e14255cc7a55dd0f2cc
porchc0 <cbporch@gmail.com>
2016-12-09 17:47:56 -0500

    Removed unused imports from Driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,18 +1,4 @@
-import opennlp.tools.coref.mention.Parse;
-import org.apache.tika.exception.TikaException;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.strings.FileConfig;
-import org.apache.tika.sax.BodyContentHandler;
-import org.mindrot.jbcrypt.BCrypt;
-import org.xml.sax.SAXException;
 import scanner.LoginGUI;
-import scanner.analysis.EmailTextGUI;
-import scanner.dbEntry.DatabaseInput;
-import scanner.filtering.Hasher;
-import org.apache.tika.parser.AutoDetectParser;
-import scanner.filtering.TikaWrapper;
-
-import java.io.*;
 
 /**
  * Created by Chris on 10/28/2016.
