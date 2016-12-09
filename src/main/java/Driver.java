11a70faff11e6035769c5487666b62269f099de7
porchc0 <cbporch@gmail.com>
2016-12-09 17:43:12 -0500

    Added TikaWrapper

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,17 +1,37 @@
+import opennlp.tools.coref.mention.Parse;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.strings.FileConfig;
+import org.apache.tika.sax.BodyContentHandler;
 import org.mindrot.jbcrypt.BCrypt;
+import org.xml.sax.SAXException;
 import scanner.LoginGUI;
 import scanner.analysis.EmailTextGUI;
 import scanner.dbEntry.DatabaseInput;
 import scanner.filtering.Hasher;
+import org.apache.tika.parser.AutoDetectParser;
+import scanner.filtering.TikaWrapper;
+
+import java.io.*;
 
 /**
  * Created by Chris on 10/28/2016.
  */
 public class Driver {
+
     public static void main(String[] args){
         LoginGUI loginGUI = new LoginGUI();
+        TikaWrapper t = new TikaWrapper();
+        try {
+            t.parseToTXT(t.stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TikaException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
         loginGUI.main(null);
     }
-
 
 }
